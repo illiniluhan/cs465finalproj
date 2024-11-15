5397bce786143a32233ae09a8dda36609c089c16
illiniluhan <luhanz2@illinois.edu>
2024-11-14 22:21:15 -0600

    Read overview and content page
    Some features cannot work(just the brightness), but I am too tired to change. Otherwise work well.

---
+++ readsbs/app/src/main/java/com/example/readsbs/ContentActivity.java
@@ -0,0 +1,297 @@
+package com.example.readsbs;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.view.WindowManager;
+
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.DocumentReference;
+public class ContentActivity extends AppCompatActivity {
+    private TextView text;
+    private Spinner fontSpinner;
+    private Spinner colorSpinner;
+    private Spinner modeSpinner;
+    private SeekBar textSizeSeekBar;
+    private SeekBar brightnessSeekBar;
+    private TextView content;
+    private ConstraintLayout container;
+    private Button setting;
+    private boolean isExpanded = false;
+    private Button back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        String sectionData = getIntent().getStringExtra("section_data");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content);
+        text = findViewById(R.id.title);
+        text.setText(sectionData);
+        back = findViewById(R.id.back_btn);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        setting = findViewById(R.id.setting_btn);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isExpanded) {
+                    fontSpinner.setVisibility(View.GONE);
+                    colorSpinner.setVisibility(View.GONE);
+                    modeSpinner.setVisibility(View.GONE);
+                    brightnessSeekBar.setVisibility(View.GONE);
+                    textSizeSeekBar.setVisibility(View.GONE);
+                } else {
+                    fontSpinner.setVisibility(View.VISIBLE);
+                    colorSpinner.setVisibility(View.VISIBLE);
+                    modeSpinner.setVisibility(View.VISIBLE);
+                    brightnessSeekBar.setVisibility(View.VISIBLE);
+                    textSizeSeekBar.setVisibility(View.VISIBLE);
+                }
+                isExpanded = !isExpanded;
+            }
+        });
+
+        fontSpinner = findViewById(R.id.font_selection);
+        setupFontSpinner(fontSpinner);
+
+        colorSpinner = findViewById(R.id.color_selection);
+        setupColorSpinner(colorSpinner);
+
+        modeSpinner = findViewById(R.id.mode_selection);
+        setupModeSpinner(modeSpinner);
+
+        content = findViewById(R.id.read_content);
+        textSizeSeekBar = findViewById(R.id.textsize_seekbar);
+        textSizeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int textSize = progress;
+                content.setTextSize(textSize);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        brightnessSeekBar = findViewById(R.id.brightness_seekbar);
+        brightnessSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                float brightness = progress / 100.0f;
+                WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+                layoutParams.screenBrightness = brightness;
+                getWindow().setAttributes(layoutParams);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+
+    private void setupFontSpinner(Spinner fontSpinner) {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.fonts_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fontSpinner.setAdapter(adapter);
+        fontSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedFont = parent.getItemAtPosition(position).toString();
+                switch (selectedFont) {
+                    case "Choose Font":
+                        Toast.makeText(ContentActivity.this, "Please choose font", Toast.LENGTH_SHORT).show();
+                        break;
+                    case "New York":
+                        Typeface typeface1 = ResourcesCompat.getFont(ContentActivity.this, R.font.newyork);
+                        content.setTypeface(typeface1);
+                        break;
+                    case "Bionic":
+                        Typeface typeface2 = ResourcesCompat.getFont(ContentActivity.this, R.font.bionic);
+                        content.setTypeface(typeface2);
+                        break;
+                    case "Default":
+                        Typeface typeface3 = ResourcesCompat.getFont(ContentActivity.this, R.font.timesnewroman);
+                        content.setTypeface(typeface3);
+                    default:
+                        Typeface typeface4 = ResourcesCompat.getFont(ContentActivity.this, R.font.timesnewroman);
+                        content.setTypeface(typeface4);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    private void setupColorSpinner(Spinner colorSpinner) {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.color_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        colorSpinner.setAdapter(adapter);
+        colorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedColor = parent.getItemAtPosition(position).toString();
+                switch (selectedColor) {
+                    case "Choose Background Color":
+                        Toast.makeText(ContentActivity.this, "Please choose background color", Toast.LENGTH_SHORT).show();
+                        break;
+                    case "Yellow":
+                        container = findViewById(R.id.content_container);
+                        container.setBackgroundColor(Color.YELLOW);
+                        fontSpinner.setBackgroundColor(Color.YELLOW);
+                        colorSpinner.setBackgroundColor(Color.YELLOW);
+                        modeSpinner.setBackgroundColor(Color.YELLOW);
+                        textSizeSeekBar.setBackgroundColor(Color.YELLOW);
+                        brightnessSeekBar.setBackgroundColor(Color.YELLOW);
+                        break;
+                    case "Green":
+                        container = findViewById(R.id.content_container);
+                        container.setBackgroundColor(Color.GREEN);
+                        fontSpinner.setBackgroundColor(Color.GREEN);
+                        colorSpinner.setBackgroundColor(Color.GREEN);
+                        modeSpinner.setBackgroundColor(Color.GREEN);
+                        textSizeSeekBar.setBackgroundColor(Color.GREEN);
+                        brightnessSeekBar.setBackgroundColor(Color.GREEN);
+                        break;
+                    case "Black":
+                        container = findViewById(R.id.content_container);
+                        container.setBackgroundColor(Color.BLACK);
+                        fontSpinner.setBackgroundColor(Color.BLACK);
+                        colorSpinner.setBackgroundColor(Color.BLACK);
+                        modeSpinner.setBackgroundColor(Color.BLACK);
+                        textSizeSeekBar.setBackgroundColor(Color.BLACK);
+                        brightnessSeekBar.setBackgroundColor(Color.BLACK);
+                        break;
+                    default:
+                        container = findViewById(R.id.content_container);
+                        container.setBackgroundColor(Color.WHITE);
+                        fontSpinner.setBackgroundColor(Color.WHITE);
+                        colorSpinner.setBackgroundColor(Color.WHITE);
+                        modeSpinner.setBackgroundColor(Color.WHITE);
+                        textSizeSeekBar.setBackgroundColor(Color.WHITE);
+                        brightnessSeekBar.setBackgroundColor(Color.WHITE);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    private void setupModeSpinner(Spinner modeSpinner) {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.mode_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        modeSpinner.setAdapter(adapter);
+        modeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedMode = parent.getItemAtPosition(position).toString();
+                switch (selectedMode) {
+                    case "Choose Mode":
+                        Toast.makeText(ContentActivity.this, "Please choose Mode", Toast.LENGTH_SHORT).show();
+                        break;
+                    case "Speed":
+                        Typeface typeface1 = ResourcesCompat.getFont(ContentActivity.this, R.font.newyork);
+                        content.setTypeface(typeface1);
+                        container = findViewById(R.id.content_container);
+                        container.setBackgroundColor(Color.WHITE);
+                        fontSpinner.setBackgroundColor(Color.WHITE);
+                        colorSpinner.setBackgroundColor(Color.WHITE);
+                        modeSpinner.setBackgroundColor(Color.WHITE);
+                        textSizeSeekBar.setBackgroundColor(Color.WHITE);
+                        brightnessSeekBar.setBackgroundColor(Color.WHITE);
+                        float brightness = 80 / 100.0f;
+                        WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+                        layoutParams.screenBrightness = brightness;
+                        getWindow().setAttributes(layoutParams);
+                        content.setTextSize(12);
+                        break;
+                    case "Bionic":
+                        Typeface typeface2 = ResourcesCompat.getFont(ContentActivity.this, R.font.bionic);
+                        content.setTypeface(typeface2);
+                        container = findViewById(R.id.content_container);
+                        container.setBackgroundColor(Color.WHITE);
+                        fontSpinner.setBackgroundColor(Color.WHITE);
+                        colorSpinner.setBackgroundColor(Color.WHITE);
+                        modeSpinner.setBackgroundColor(Color.WHITE);
+                        textSizeSeekBar.setBackgroundColor(Color.WHITE);
+                        brightnessSeekBar.setBackgroundColor(Color.WHITE);
+                        float brightness2 = 80 / 100.0f;
+                        WindowManager.LayoutParams layoutParam2 = getWindow().getAttributes();
+                        layoutParam2.screenBrightness = brightness2;
+                        getWindow().setAttributes(layoutParam2);
+                        content.setTextSize(16);
+                        break;
+                    default:
+                        Typeface typeface4 = ResourcesCompat.getFont(ContentActivity.this, R.font.timesnewroman);
+                        content.setTypeface(typeface4);
+                        container = findViewById(R.id.content_container);
+                        container.setBackgroundColor(Color.WHITE);
+                        fontSpinner.setBackgroundColor(Color.WHITE);
+                        colorSpinner.setBackgroundColor(Color.WHITE);
+                        modeSpinner.setBackgroundColor(Color.WHITE);
+                        textSizeSeekBar.setBackgroundColor(Color.WHITE);
+                        brightnessSeekBar.setBackgroundColor(Color.WHITE);
+                        float brightness3 = 80 / 100.0f;
+                        WindowManager.LayoutParams layoutParam3 = getWindow().getAttributes();
+                        layoutParam3.screenBrightness = brightness3;
+                        getWindow().setAttributes(layoutParam3);
+                        content.setTextSize(14);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+
+    }
+}
--- readsbs/app/src/main/java/com/example/readsbs/ReadActivity.java
+++ readsbs/app/src/main/java/com/example/readsbs/ReadActivity.java
@@ -3,6 +3,7 @@ package com.example.readsbs;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -12,10 +13,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ReadActivity extends AppCompatActivity {
 
-    private LinearLayout bookSection;
-    private LinearLayout bookSectionDetails;
-    private LinearLayout bookSectionDetails2;
-    private LinearLayout bookSectionDetails3;
+    private LinearLayout bookSection1;
+    private LinearLayout bookSectionDetails11;
+    private LinearLayout bookSectionDetails12;
+    private LinearLayout bookSectionDetails13;
+
+    private LinearLayout bookSection2;
+    private LinearLayout bookSectionDetails21;
+    private LinearLayout bookSectionDetails22;
+    private LinearLayout bookSectionDetails23;
+    private LinearLayout bookSectionDetails24;
+
+    private Button readsec11;
+    private Button readsec12;
+    private Button readsec13;
+    private Button readsec21;
+    private Button readsec22;
+    private Button readsec23;
+    private Button readsec24;
     private boolean isExpanded = false;
 
     @Override
@@ -25,25 +40,100 @@ public class ReadActivity extends AppCompatActivity {
 
         RadioGroup bottomNavigation = findViewById(R.id.bottom_navigation);
 
-        bookSection = findViewById(R.id.book_section);
-        bookSectionDetails = findViewById(R.id.book_section_details);
-        bookSectionDetails2 = findViewById(R.id.book_section_details2);
-        bookSectionDetails3 = findViewById(R.id.book_section_details3);
+        bookSection1 = findViewById(R.id.book1);
+        bookSectionDetails11 = findViewById(R.id.book_section_details11);
+        bookSectionDetails12 = findViewById(R.id.book_section_details12);
+        bookSectionDetails13 = findViewById(R.id.book_section_details13);
 
-        bookSection.setOnClickListener(new View.OnClickListener() {
+        bookSection2 = findViewById(R.id.book2);
+        bookSectionDetails21 = findViewById(R.id.book_section_details21);
+        bookSectionDetails22 = findViewById(R.id.book_section_details22);
+        bookSectionDetails23 = findViewById(R.id.book_section_details23);
+        bookSectionDetails24 = findViewById(R.id.book_section_details24);
+
+        readsec11 = findViewById(R.id.read_btn11);
+        readsec12 = findViewById(R.id.read_btn12);
+        readsec13 = findViewById(R.id.read_btn13);
+        readsec21 = findViewById(R.id.read_btn21);
+        readsec22 = findViewById(R.id.read_btn22);
+        readsec23 = findViewById(R.id.read_btn23);
+        readsec24 = findViewById(R.id.read_btn24);
+
+        bookSection1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (isExpanded) {
-                    bookSectionDetails.setVisibility(View.GONE);
-                    bookSectionDetails2.setVisibility(View.GONE);
-                    bookSectionDetails3.setVisibility(View.GONE);
+                    bookSectionDetails11.setVisibility(View.GONE);
+                    bookSectionDetails12.setVisibility(View.GONE);
+                    bookSectionDetails13.setVisibility(View.GONE);
                 } else {
-                    bookSectionDetails.setVisibility(View.VISIBLE);
-                    bookSectionDetails2.setVisibility(View.VISIBLE);
-                    bookSectionDetails3.setVisibility(View.VISIBLE);
+                    bookSectionDetails11.setVisibility(View.VISIBLE);
+                    bookSectionDetails12.setVisibility(View.VISIBLE);
+                    bookSectionDetails13.setVisibility(View.VISIBLE);
                 }
                 isExpanded = !isExpanded;
             }
+        });
+
+        bookSection2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isExpanded) {
+                    bookSectionDetails21.setVisibility(View.GONE);
+                    bookSectionDetails22.setVisibility(View.GONE);
+                    bookSectionDetails23.setVisibility(View.GONE);
+                    bookSectionDetails24.setVisibility(View.GONE);
+                } else {
+                    bookSectionDetails21.setVisibility(View.VISIBLE);
+                    bookSectionDetails22.setVisibility(View.VISIBLE);
+                    bookSectionDetails23.setVisibility(View.VISIBLE);
+                    bookSectionDetails24.setVisibility(View.VISIBLE);
+                }
+                isExpanded = !isExpanded;
+            }
+        });
+
+        readsec11.setOnClickListener(v -> {
+            Intent intent = new Intent(ReadActivity.this, ContentActivity.class);
+            intent.putExtra("section_data", "section11");
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+        readsec12.setOnClickListener(v -> {
+            Intent intent = new Intent(ReadActivity.this, ContentActivity.class);
+            intent.putExtra("section_data", "section12");
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+        readsec13.setOnClickListener(v -> {
+            Intent intent = new Intent(ReadActivity.this, ContentActivity.class);
+            intent.putExtra("section_data", "section13");
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+        readsec21.setOnClickListener(v -> {
+            Intent intent = new Intent(ReadActivity.this, ContentActivity.class);
+            intent.putExtra("section_data", "section21");
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+        readsec22.setOnClickListener(v -> {
+            Intent intent = new Intent(ReadActivity.this, ContentActivity.class);
+            intent.putExtra("section_data", "section22");
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+        readsec23.setOnClickListener(v -> {
+            Intent intent = new Intent(ReadActivity.this, ContentActivity.class);
+            intent.putExtra("section_data", "section23");
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+        readsec24.setOnClickListener(v -> {
+            Intent intent = new Intent(ReadActivity.this, ContentActivity.class);
+            intent.putExtra("section_data", "section24");
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
         });
 
         bottomNavigation.setOnCheckedChangeListener((group, checkedId) -> {
