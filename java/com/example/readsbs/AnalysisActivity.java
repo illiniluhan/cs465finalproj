1e06b609f47f2f897809ded435932542912c0af4
jamesj6 <jamesj6@illinois.edu>
2024-11-16 00:20:55 -0600

    Reading analysis and progress pages

---
+++ java/com/example/readsbs/AnalysisActivity.java
@@ -0,0 +1,49 @@
+package com.example.readsbs;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AnalysisActivity extends AppCompatActivity {
+    private TextView read_progress;
+    private TextView read_analysis;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analysis);
+
+        read_progress = findViewById(R.id.reading_prog);
+        read_analysis = findViewById(R.id.read_analy);
+
+        read_progress.setOnClickListener(v -> {
+            Intent intent = new Intent(AnalysisActivity.this, AnalysisProgress.class);
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+
+        read_analysis.setOnClickListener(v -> {
+            Intent intent = new Intent(AnalysisActivity.this, AnalysisAnalysis.class);
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+
+        RadioGroup bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        bottomNavigation.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == R.id.navigation_analysis) {
+            } else if (checkedId == R.id.navigation_import) {
+                Toast.makeText(AnalysisActivity.this, "Redirecting", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(AnalysisActivity.this, MainActivity.class));
+            } else if (checkedId == R.id.navigation_read) {
+                Toast.makeText(AnalysisActivity.this, "Redirecting", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(AnalysisActivity.this, ReadActivity.class));
+            }
+        });
+        ((RadioButton) findViewById(R.id.navigation_analysis)).setChecked(true);
+    }
+}
---
+++ java/com/example/readsbs/ReadActivity.java
@@ -0,0 +1,174 @@
+package com.example.readsbs;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReadActivity extends AppCompatActivity {
+
+    private LinearLayout bookSection1;
+    private LinearLayout bookSectionDetails11;
+    private LinearLayout bookSectionDetails12;
+    private LinearLayout bookSectionDetails13;
+
+    private LinearLayout bookSection2;
+    private LinearLayout bookSectionDetails21;
+    private LinearLayout bookSectionDetails22;
+    private LinearLayout bookSectionDetails23;
+    private LinearLayout bookSectionDetails24;
+
+    private Button readsec11;
+    private Button readsec12;
+    private Button readsec13;
+    private Button readsec21;
+    private Button readsec22;
+    private Button readsec23;
+    private Button readsec24;
+    private boolean isExpanded = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read);
+
+        RadioGroup bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        ImageView alarmLogo1 = findViewById(R.id.alarm_logo_1);
+        ImageView alarmLogo2 = findViewById(R.id.alarm_logo_2);
+        ImageView alarmLogo3 = findViewById(R.id.alarm_logo_3);
+        ImageView alarmLogo4 = findViewById(R.id.alarm_logo_4);
+        ImageView alarmLogo5 = findViewById(R.id.alarm_logo_5);
+        ImageView alarmLogo6 = findViewById(R.id.alarm_logo_6);
+        ImageView alarmLogo7 = findViewById(R.id.alarm_logo_7);
+        View.OnClickListener alarmClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ReadActivity.this, ReminderActivity.class);
+                startActivity(intent);
+            }
+        };
+        alarmLogo1.setOnClickListener(alarmClickListener);
+        alarmLogo2.setOnClickListener(alarmClickListener);
+        alarmLogo3.setOnClickListener(alarmClickListener);
+        alarmLogo4.setOnClickListener(alarmClickListener);
+        alarmLogo5.setOnClickListener(alarmClickListener);
+        alarmLogo6.setOnClickListener(alarmClickListener);
+        alarmLogo7.setOnClickListener(alarmClickListener);
+
+        bookSection1 = findViewById(R.id.book1);
+        bookSectionDetails11 = findViewById(R.id.book_section_details11);
+        bookSectionDetails12 = findViewById(R.id.book_section_details12);
+        bookSectionDetails13 = findViewById(R.id.book_section_details13);
+
+        bookSection2 = findViewById(R.id.book2);
+        bookSectionDetails21 = findViewById(R.id.book_section_details21);
+        bookSectionDetails22 = findViewById(R.id.book_section_details22);
+        bookSectionDetails23 = findViewById(R.id.book_section_details23);
+        bookSectionDetails24 = findViewById(R.id.book_section_details24);
+
+        readsec11 = findViewById(R.id.read_btn11);
+        readsec12 = findViewById(R.id.read_btn12);
+        readsec13 = findViewById(R.id.read_btn13);
+        readsec21 = findViewById(R.id.read_btn21);
+        readsec22 = findViewById(R.id.read_btn22);
+        readsec23 = findViewById(R.id.read_btn23);
+        readsec24 = findViewById(R.id.read_btn24);
+
+        bookSection1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isExpanded) {
+                    bookSectionDetails11.setVisibility(View.GONE);
+                    bookSectionDetails12.setVisibility(View.GONE);
+                    bookSectionDetails13.setVisibility(View.GONE);
+                } else {
+                    bookSectionDetails11.setVisibility(View.VISIBLE);
+                    bookSectionDetails12.setVisibility(View.VISIBLE);
+                    bookSectionDetails13.setVisibility(View.VISIBLE);
+                }
+                isExpanded = !isExpanded;
+            }
+        });
+
+        bookSection2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isExpanded) {
+                    bookSectionDetails21.setVisibility(View.GONE);
+                    bookSectionDetails22.setVisibility(View.GONE);
+                    bookSectionDetails23.setVisibility(View.GONE);
+                    bookSectionDetails24.setVisibility(View.GONE);
+                } else {
+                    bookSectionDetails21.setVisibility(View.VISIBLE);
+                    bookSectionDetails22.setVisibility(View.VISIBLE);
+                    bookSectionDetails23.setVisibility(View.VISIBLE);
+                    bookSectionDetails24.setVisibility(View.VISIBLE);
+                }
+                isExpanded = !isExpanded;
+            }
+        });
+
+        readsec11.setOnClickListener(v -> {
+            Intent intent = new Intent(ReadActivity.this, ContentActivity.class);
+            intent.putExtra("section_data", "section11");
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+        readsec12.setOnClickListener(v -> {
+            Intent intent = new Intent(ReadActivity.this, ContentActivity.class);
+            intent.putExtra("section_data", "section12");
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+        readsec13.setOnClickListener(v -> {
+            Intent intent = new Intent(ReadActivity.this, ContentActivity.class);
+            intent.putExtra("section_data", "section13");
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+        readsec21.setOnClickListener(v -> {
+            Intent intent = new Intent(ReadActivity.this, ContentActivity.class);
+            intent.putExtra("section_data", "section21");
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+        readsec22.setOnClickListener(v -> {
+            Intent intent = new Intent(ReadActivity.this, ContentActivity.class);
+            intent.putExtra("section_data", "section22");
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+        readsec23.setOnClickListener(v -> {
+            Intent intent = new Intent(ReadActivity.this, ContentActivity.class);
+            intent.putExtra("section_data", "section23");
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+        readsec24.setOnClickListener(v -> {
+            Intent intent = new Intent(ReadActivity.this, ContentActivity.class);
+            intent.putExtra("section_data", "section24");
+            Toast.makeText(this, "Redirecting", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        });
+
+        bottomNavigation.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == R.id.navigation_read) {
+            } else if (checkedId == R.id.navigation_import) {
+                Toast.makeText(ReadActivity.this, "Redirecting", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(ReadActivity.this, MainActivity.class));
+            } else if (checkedId == R.id.navigation_analysis) {
+                Toast.makeText(ReadActivity.this, "Redirecting", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(ReadActivity.this, AnalysisActivity.class));
+            }
+        });
+        ((RadioButton) findViewById(R.id.navigation_read)).setChecked(true);
+    }
+}
